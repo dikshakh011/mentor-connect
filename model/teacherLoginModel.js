12d278391e38d47c1a7626f214f3e09e9f1dbaf2
Diksha Khetan <khetandiksha11d@gmail.com>
2025-05-15 19:39:52 +0530

    deletion successfull

--- model/teacherLoginModel.js
+++
@@ -1,7 +0,0 @@
-const {model} = require("mongoose");
-
-const {teacherLoginSchema} = require("../schema/teacherLoginSchema");
-
-const teacherLoginModel = new model("teacher",teacherLoginSchema);
-
-module.exports = {teacherLoginModel};
